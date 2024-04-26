fb0ba26a6290bc8d9508caff24e1e33d0aa5b298
Alireza <104611074+pvgorg@users.noreply.github.com>
2024-04-26 07:51:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import psutil , sqlite3 , random
 from pyrogram import Client, filters 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client, filters, errors
-import time
+from pathlib import Path
 
-admin = 141374540
+
+admin = 1502490631
 api_id = 23136380
 api_hash = '6ae6541159e229499615953de667675c'
 
+
 client = Client("self", api_id, api_hash)
-time = None
-target_id = None
 scheduler = AsyncIOScheduler()
 cnx = sqlite3.connect("data.db")
 cursor = cnx.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS bot(model TEXT DEFAULT 'text' , tag TEXT DEFAULT 'off', setid TEXT , setgp TEXT , texttag TEXT , repcht INTEGER , repmes INTEGER ,  time INTEGER DEFAULT '1')''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS bot(model TEXT DEFAULT 'text' , tag TEXT DEFAULT 'off', setid TEXT , setgp TEXT , texttag TEXT , repcht INTEGER , repmes INTEGER , enchat INTEGER , enmes INTEGER ,  time INTEGER DEFAULT '1')''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS listfosh(fosh TEXT)''')
 cursor.execute('''CREATE TABLE IF NOT EXISTS listadmin(admin TEXT)''')
 cursor.execute(f"SELECT * FROM listadmin WHERE admin = '{admin}'")
@@ -38,6 +38,12 @@ async def jobfosh(model , chatid , tag , Userid , textfosh):
             await client.send_message(chatid,  text= f'[{textfosh}](tg://user?id={int(Userid)}){fosh}')
         elif tag == 'off':
             await client.send_message(chatid,  text= fosh)
+
+async def jobenemi(chatid,mesid):
+    listfosh = getdata('fosh','listfosh')
+    fosh = random.choice(listfosh)
+
+    await client.send_message(chatid , reply_to_message_id = mesid ,  text= fosh)
 
 
 def senddata(data , value , namevalue):
@@ -66,36 +72,28 @@ def getdata(value , table):
             list.append(row[0])
     return list
 # =========== Handlers =========== #
-@client.on_message(filters.text)
+@client.on_message(filters.text , filters.photo)
 async def spamer_handler(client, message):
 
-    global target_id
     text = message.text
     Userid = message.from_user.id
     listadmin = getdata('admin','listadmin')
 
     if str(Userid) in listadmin:
         if text == "bot":
-            await message.reply_text('ربات اتکر علیرضا ولف آنلاین هست' , quote = True)
+            await message.reply_text('ربات علیرضا ولف فعال است!' , quote = True)
 
-        if text == "start":
-            await message.reply_text('Ok.!' , quote = True)
-            
-        if text == "داش":
-            await message.reply_text('داشمی ❤' , quote = True)
-         
-  
         elif text == "help2":
             await message.reply_text("""🌵 راهنمای سلف
     ❋ `admin`(id) ⤳ با استفاده از دستور زیر ادمین جدید اضافه کنید به اتکر  
     
     ❋ `deladmin`(id) ⤳ با استفاده از دستور زیر ادمین مورد نظر را حذف کنی   
     
-    ❋ `delalladmin` ⤳ با استفاده از دستور زیر تمام ادمین ها را حذف کنید    
-                
+    ❋ `delalladmin` ⤳ با استفاده از دستور زیر تمام ادمین ها را حذف کنید     
+               
     ❋ `listadmin` ⤳ با استفاده از دستور زیر لیست ادمین ها را مشاهده کنید 
         
-    ❋ owner ⤳  <b>Coder By Alireza Wolf</b>  🧑‍💻
+    ❋  <b>Coded By Alireza Wolf</b>
             """)
 
         elif text == "help":
@@ -104,39 +102,45 @@ async def spamer_handler(client, message):
     
     ❋ `stats` ⤳ اطلاع از وضعیت کلی بات
     
-    ❋ `id` (rep) ⤳ دریافت ایدی گپ و فرد مقابل     
-                
+    ❋ `id` (rep) ⤳ دریافت ایدی گپ و فرد مقابل    
+                 
     ❋ `mode` (text/rep) ⤳ دستوره تغییر مدل ارسال پیام
     
-    ❋ `setid` (id) ⤳ ست کردن ایدی شخص برای میشن     
-                       
+    ❋ `setid` (id) ⤳ ست کردن ایدی شخص برای میشن  
+                          
     ❋ `setgp` (id gp) ⤳ ست کردن گروه برای ارسال پیام
     
     ❋ `setrep` (id gp) ⤳ ست کردن پیامی پست برای فروارد 
     
-    ❋ `settime` (1~99999) ⤳ تنظیم زمان ارسال پیام به گروه یا پیوی     
-                           
-    ❋ `addfosh` ⤳ اضافه کردن فوش مورد نظر
+    ❋ `settime` (1~99999) ⤳ تنظیم زمان ارسال پیام به گروه یا پیوی      
+  
+    ❋ `setenemy`reply ⤳ تنظیم پیام برا اتک   
+                     
+    ❋ `addfosh` reply ⤳ اضافه کردن فوش مورد نظر
     
-    ❋ `delallfosh` ⤳ حذف تمامی فوش های ربات     
-                                                      
+    ❋ `delallfosh` ⤳ حذف تمامی فوش های ربات
+                                                           
     ❋ `tag` (on/off) ⤳ روشن خاموش کردن میشن کردن شخص
     
     ❋ `fosh`(on/off)⤳ ارسال پیام به گروه یا پیوی
     
+    ❋ `attack`(on/off)⤳ ارسال رگبار بر روی پیام       
+                     
     ❋ `join` @telegram ⤳ جوین شدن در گروه ها
-    
     ❋ `left` @telegram ⤳ لفت دادن از گروه مورد نظر
     
-    ❋ `texttag` ⤳ ست کردن متن برای میشن   
-                                        
-    ❋ `help2` ⤳ باز کردن راهنمای افزودن ادمین به ربات   
-                             
-    ❋ owner ⤳ @PvGoRg 🧑‍💻
+    ❋ `texttag` ⤳ ست کردن متن برای میشن 
+ 
+    ❋ `setphoto` ⤳ تغییر عکس پروفایل
+    
+
+
+    ❋ `help2` ⤳ باز کردن راهنمای افزودن ادمین به ربات                                             
+    ❋ owner ⤳ @La_shy 🧑‍💻
+    
+       V : 6.9.12
             """)
         
-
-    
         elif text == "id":
             if message.reply_to_message is not None:
                 if message.reply_to_message.forward_from is not None:
@@ -193,7 +197,17 @@ async def spamer_handler(client, message):
                 respons = senddata(data= getdata('texttag','bot') , value=text , namevalue= 'texttag')
                 await message.reply_text(f'{respons}')
             else:
-                await message.reply_text('لطفا متن بعد `texttag` بفرستید \n مثال : texttag dev_Atakeri')
+                await message.reply_text('لطفا متن بعد `texttag` بفرستید \n مثال : texttag Atakeri')
+
+        elif text == 'setenemy':
+
+            if message.reply_to_message is not None:
+                    senddata(data= getdata('enchat' , 'bot') , namevalue= 'enchat' , value= message.reply_to_message.chat.id)
+                    senddata(data= getdata('enmes' , 'bot') , namevalue= 'enmes' , value= message.reply_to_message.id)
+                    await message.reply_text(f'پیام مورد نطر شما به عنوان انمی ذخیره شد')
+                    
+            elif message.reply_to_message is  None:
+                await message.reply('روی پیام مورد نطر ریپلای کن') 
 
         elif text.startswith("settime"):
             text = text.split()
@@ -254,19 +268,49 @@ async def spamer_handler(client, message):
 
             await message.reply_text(f"{txt}")
 
+        elif text.startswith('attack'):
 
-
-        elif text.startswith('addfosh'):
             text = text.split()
 
-            if len(text) >= 2:
-                text = ' '.join((text[1:]))
+            if len(text) == 2 and text[1] == "on":
+                time = getdata('time' , 'bot')
+
+                try:
+                    time = int(time[0])
+                except ValueError:
+                    return
+                
+                this_job = scheduler.get_job(job_id=f"job_enemi")
+                if this_job is not None:
+                    scheduler.remove_job(job_id=f"job_enemi")
+                scheduler.add_job(jobenemi, "interval", seconds= time , id=f"job_enemi" ,args=(getdata('enchat' , 'bot')[0] , getdata('enmes' , 'bot')[0]))
+                txt = f"حالت اتک روشن شد"
+
+
+            elif len(text) == 2 and text[1] == "off":
+
+                this_job = scheduler.get_job(job_id=f"job_enemi")
+                if this_job is None:
+                    txt = "حالت اتک خاموش بوده است"
+                else:
+                    scheduler.remove_job(job_id=f"job_enemi")
+                    txt = " حالت اتک خاموش شد"
+
+            await message.reply_text(f"{txt}")
+
+        elif text == 'addfosh':
+
+            
+            if message.reply_to_message is not None:
+
+                text = message.reply_to_message.text.replace('\n', '\n')
                 cursor.execute("""INSERT INTO listfosh(fosh) VALUES (?)""" , (text,))
                 cnx.commit()
                 await message.reply_text(f'فوش شما ست شد \n فوش: {text}')
                 return
-
-            await message.reply_text('لطفا فوش بعد `addfosh` بفرستید \n مثال : addfosh سلام')
+            
+            elif message.reply_to_message is not None:
+                await message.reply_text('روی متن فوش ریپلای بزن')
 
         
         elif text == 'delallfosh':
@@ -353,6 +397,16 @@ async def spamer_handler(client, message):
 
             await message.reply(f"لیست ادمیها \n\n {listadmin}")
 
+        elif text.startswith('setphoto'):
+
+            if message.reply_to_message is not None:
+                await message.reply_to_message.download(file_name="image.jpg")
+                await client.set_profile_photo(photo=f"{Path.cwd()}\downloads\image.jpg") 
+            
+            if message.reply_to_message is  None:
+                await message.reply('روی عکس مورد نظر ریپلای کن')
+
+    
     elif Userid not in listadmin:
         return
 
