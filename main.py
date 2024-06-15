16893f7145836ca673e4b4431c8ce0f3ae2206ef
Alireza <104611074+pvgorg@users.noreply.github.com>
2024-06-15 13:24:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ async def spamer_handler(client, message):
 
             if message.reply_to_message is not None:
                 await message.reply_to_message.download(file_name="image.jpg")
-                await client.set_profile_photo(photo=f"/root/@mehrab_11Bot/bot-spam/downloads/image.jpg") #edit adrres
+                await client.set_profile_photo(photo=f"/root/mehrab_11Bot/bot-spam/downloads/image.jpg") #edit adrres
                 await message.reply('عکس با موفقیت تنطیم شد')
 
 
